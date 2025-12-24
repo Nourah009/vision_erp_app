2da783f9855bdcabc33da4a9faa3e5b952ee65c5
Amer Alfaqeeh <amer.alfaqeeh@hotmail.com>
2025-12-25 01:29:54 +0300

    Update2-1

--- lib/core/di/injection_container.dart
+++ lib/core/di/injection_container.dart
@@ -1,11 +1,16 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:get_it/get_it.dart';
 import 'package:vision_erp_app/core/network/network_info.dart';
+import 'package:vision_erp_app/core/platform/platform_service.dart';
+import 'package:vision_erp_app/core/storage/prefs_storage_service.dart';
+import 'package:vision_erp_app/core/storage/secure_storage_service.dart';
+import 'package:vision_erp_app/core/storage/storage_service.dart';
 import 'package:vision_erp_app/features/auth/data/datasources/remote/auth_remote_data_source.dart';
 import 'package:vision_erp_app/features/auth/data/repositories/auth_repository_impl.dart';
 import 'package:vision_erp_app/features/auth/domain/repositories/auth_repository.dart';
 import 'package:vision_erp_app/features/auth/domain/usecases/login.dart';
 import 'package:vision_erp_app/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 final sl = GetIt.instance;
 
@@ -31,6 +36,13 @@ void init() {
 
   // Core
   sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(sl()));
+  sl.registerLazySingleton(() => PlatformService());
+
+  if (kIsWeb) {
+    sl.registerLazySingleton<StorageService>(() => PrefsStorageService());
+  } else {
+    sl.registerLazySingleton<StorageService>(() => SecureStorageService());
+  }
 
   // External
   sl.registerLazySingleton(() => Connectivity());
